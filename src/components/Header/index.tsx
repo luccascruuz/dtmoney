cb8b762e05e5565c672577fecdc920f52b716594
luccascruuz <luccas.185@hotmail.com>
2021-12-06 18:09:55 -0300

    feat: criacao da modal global

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,18 @@
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionsModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionsModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">Nova transação</button>
+        <button onClick={onOpenNewTransactionsModal} type="button">
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
